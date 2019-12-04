daceca998928eda5eeb388a03c739b20b565b892
NineSwordsMonster <wangjia_1919@163.com>
2019-12-04 09:05:20 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { path } = require('@vuepress/shared-utils')
 
 module.exports = (options, context) => ({
-  name: '@nines/plugin-pagation',
+  name: '@nines/vuepress-plugin-pagation',
   enhanceAppFiles: [
     path.resolve(__dirname, './bin/enhanceAppFile.js')
   ]
